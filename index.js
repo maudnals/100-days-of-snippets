04b6db78a98dba29c783ed44317caee980a655b8
D068996 <m.nalpas@sap.com>
2018-01-08 18:24:22 +0100

    commented conditional return

---
+++ index.js
@@ -0,0 +1,22 @@
+
+/* 
+# 001
+Tags: object, prototype
+Lib/Fwk: Redux
+Source: https://github.com/reactjs/redux/blob/master/src/utils/isPlainObject.js 
+*/
+
+export default function isPlainObject(obj) {
+  if (typeof obj !== "object" || obj === null) {
+    // check obj is of OBJECT type: 
+    // its type must be "object" (all primitives have their stringified type as type, including undefined)
+    // AND it must not be "null" (JS bug: typeof null = "object")
+    return false;
+    // if obj is not of object type, then we already know it's not a plain object.
+    // execution stops here and false is returned.
+  }
+  let proto = Object.getPrototypeOf(obj);
+  // "Object" is JS's built-in Base Object.
+  // proto is 
+  // while (proto !== null)
+}
