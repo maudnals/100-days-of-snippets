b1daf9429d91833e27b7e4711a8442f2e35332e3
maudnals <maud.nalpas@gmail.com>
2018-01-13 16:43:19 +0100

    detailed 005

--- 005.js
+++ 005.js
@@ -8,17 +8,29 @@ Source: https://github.com/reactjs/redux/blob/master/src/bindActionCreators.js
 function bindActionCreator(actionCreator, dispatch) {
   // an action creator is a f that creates an action object
   // {type: "XXX", payload: x}
-  // e.g.
-  // updateIdeasFetchStatus(ideasFetchStatus) {
-  //   return {
-  //     type: "UPDATE_IDEAS_FETCH_STATUS",
-  //     payload: ideasFetchStatus
-  //   }
-  // }
-  // wrap the actionCreator function into a dispatch call
   return function() {
-    return dispatch(actionCreator.apply(this, arguments))
+    // return a function without invoking it (the input is also a function). Here function is needed so we can wrap the invokation. In order to wrap a function b into another one a we need f() {b(a)}.
+    // return dispatch(actionCreator);
+
+    // return (actionCreator.apply());
+      // wrap the actionCreator function into a dispatch call
+    // return dispatch(actionCreator.apply(this, arguments));
   }
+}
+
+
+// To test:
+
+function actionCreatorUpdateIdea(idea) {
+  return {
+    type: "UPDATE_IDEA",
+    payload: idea
+  };
+}
+
+function dispatch(action) { 
+  /* do sthg with the action */ 
+  return action; 
 }
 
 
