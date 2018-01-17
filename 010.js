d98c8f7b109e54f88a3c5d48cbc727bbe02be6e5
maudnals <maud.nalpas@gmail.com>
2018-01-17 18:37:26 +0100

    010 part 2

--- 010.js
+++ 010.js
@@ -22,5 +22,19 @@ Source: https://github.com/webcomponents/webcomponentsjs/blob/master/webcomponen
     // if not that means we need the "hi" (= html imports) polyfill, so push it onto the array
     polyfills.push('hi');
   }
+  // [....] similar stuff
+  if (!window.customElements || window.customElements.forcePolyfill) {
+    // looks like there is an option to enforce polyfills to be applied event if window.customElements IS present - probably to polyfill a partial/shoddy implementation
+    polyfills.push('ce');
+  }
+  if (!('content' in document.createElement('template')) || !window.Promise || !Array.from ||
+    !(document.createDocumentFragment().cloneNode() instanceof DocumentFragment)) {
+    // if no templating feature
+    // or no ES6 (window.promise, Array.from)
+    // yes, Array.from() is ES6. It creates a new Array instance from an array-like/iterable object (for example an HTML NodeList!)
+    polyfills = ['lite'];
+    // need the "lite" polyfill
+    // which is "the full suite of polyfills, called `lite` for legacy reasons" (src: original code comments)
+  }
   // ........ to be continued!
 })();
