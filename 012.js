cca3458def2e059fa4e54892f5b2e9e42bdedce2
maudnals <maud.nalpas@gmail.com>
2018-01-19 22:29:08 +0100

    line wrapping

--- 012.js
+++ 012.js
@@ -11,10 +11,13 @@ Source: https://github.com/reactjs/redux/blob/master/src/createStore.js
 //   type: "HIDE_NOTIFICATION", 
 //   payload: myText
 // }
-// which makes me realize... an action should always be a verb, that's why it's called action.
-// is my "UPDATE_IS_DESCRIPTION_EXPANDED" wrong?
-// ... no, it's fine, it's just the naming and the way it's used.
-// probably it would be better to have two actions called IDEA_DESCRIPTION::EXPAND and IDEA_DESCRIPTION::COLLAPSE that would both, once dispatched, act on the same boolean isDescriptionExpanded.
+
+/* an action should always be a verb, that's why it's called action.
+Is my "UPDATE_IS_DESCRIPTION_EXPANDED" wrong?
+... no, it's fine, it's just the naming and the way it's used.
+probably it would be better to have two actions: 
+IDEA_DESCRIPTION::EXPAND and IDEA_DESCRIPTION::COLLAPSE 
+which would both, once dispatched, act on the same boolean isDescriptionExpanded.*/
 
 function dispatch(action) {
 
@@ -39,12 +42,28 @@ function dispatch(action) {
 
   if (isDispatching) {
     // if isDispatching is true, it means that: 
-    /* even though i'm currently executing a dispatch (up the stack), somewhere below in the stack there's another dispatch call. Which paused in order to execute, since JS is single threaded async. And which paused while isDispatching was true apparently, since we're entering this block. And the only place where that's possible is in the block below where isDispatching is set to true, and oh the function that's called there is a reducer. So it means dispatch was called by a reducer, hence the message new Error('Reducers may not dispatch actions.') makes sense. */
-    // OK, so this whole `isDispatching` thing is here just to prevent devs from making Reducers dispatch actions. Why do we want to avoid that?
-    // Because reducers must be pure functions and have no side effect. As documented there https://github.com/reactjs/redux/blob/master/docs/api/Store.md: 
-    // "You are disallowed to dispatch inside the reducers because they must have no side effects. If you want to cause a side effect in response to an action, the right place to do this is in the potentially async action creator." (this has nothing to do with an infinite loop as i thought, since the reducers would be different ones obviously)
-    // OK makes sense.
-    // 🏁  Side node: both dispatch() execution contexts have access to isDispatching (defined outside of dispatch()) because of closures: dispatch has closed isDispatching in. Another nice use of closure: access to the call stack's state! Wow.
+    /* even though i'm currently executing a dispatch (up the stack), somewhere below in the stack there's another dispatch call. 
+    Which paused in order to execute, since JS is single threaded async.
+    And which paused while isDispatching was true apparently, since we're entering this block. 
+    And the only place where that's possible is in the block below where isDispatching is set to true, 
+    And oh the function that's called there is a reducer. 
+    So it means dispatch was called by a reducer.
+    Hence the message new Error('Reducers may not dispatch actions.').
+    ---
+    OK, so this whole `isDispatching` thing is here just to prevent devs from making Reducers dispatch actions. 
+    Why do we want to avoid that?
+    Because reducers must be pure functions and have no side effect.
+    https://github.com/reactjs/redux/blob/master/docs/api/Store.md:
+    "You are disallowed to dispatch inside the reducers because they must have no side effects.
+    If you want to cause a side effect in response to an action, the right place to do this is in the potentially async action creator." 
+    (this has nothing to do with an infinite loop as i thought, since the reducers would be different ones obviously, 
+    // and if they are the infinite loop is on the user's side)
+    OK makes sense.
+    ---
+    🏁
+    Both dispatch() execution contexts have access to isDispatching (defined outside of dispatch()) 
+    thanks to closures: dispatch has closed isDispatching in.
+    Another nice use of closure: access to the call stack's state.*/
     throw new Error('Reducers may not dispatch actions.')
   }
 
@@ -63,10 +82,12 @@ function dispatch(action) {
   const listeners = (currentListeners = nextListeners)
   for (let i = 0; i < listeners.length; i++) {
     // call all listeners (why don't they simply listeners[i]() ? not sure.)
-    // But wait why isn't there also here a check here that isDispatching should be false?
-    // No need. As documented there https://github.com/reactjs/redux/blob/master/docs/api/Store.md: 
-    // "In Redux, subscriptions are called after the root reducer has returned the new state, so you may dispatch in the subscription listeners.
-    // OK sure, when we're here currentState has been updated already, so it's no issue calling yet another dispatch
+    /*
+    But wait why isn't there also here a check here that isDispatching should be false?
+    No need. As documented there https://github.com/reactjs/redux/blob/master/docs/api/Store.md: 
+    "In Redux, subscriptions are called after the root reducer has returned the new state, so you may dispatch in the subscription listeners.
+    OK sure, when we're here currentState has been updated already, so it's no issue calling yet another dispatch
+    */
     const listener = listeners[i]
     listener()
   }
