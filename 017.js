a0ac788e223b40217dbd04c80e0b3cc749002701
maudnals <maud.nalpas@gmail.com>
2018-01-25 08:40:24 +0100

    added bottom of createStore

--- 017.js
+++ 017.js
@@ -1,1 +1,27 @@
-// todo wednesday
+/* 
+# 017
+Tags: actions
+Lib/Fwk: redux
+Source: https://github.com/reactjs/redux/blob/master/src/createStore.js
+*/
+
+export default function createStore(reducer, preloadedState, enhancer) {
+  // getState
+  // subscribe
+  // dispatch
+  // misc: observable, replaceReducer, etc.
+
+  // Before returning the created store the INIT action is dispatched
+  // Which calls:
+  // currentState = currentReducer(currentState, action)
+  // currentState = preloadedState, action = init, currentReducer = reducer
+  // that fills the initial state tree
+  dispatch({ type: ActionTypes.INIT });
+  return {
+    dispatch,
+    subscribe,
+    getState,
+    replaceReducer,
+    [$$observable]: observable
+  }
+}
