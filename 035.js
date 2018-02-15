ce6e735298a72475d62dbf05ffd85c27e30a2ebc
maudnals <maud.nalpas@gmail.com>
2018-02-15 08:27:41 +0100

    commented on curryng

--- 035.js
+++ 035.js
@@ -1,6 +1,6 @@
 /* 
 # 035
-Tags: framework main function
+Tags: framework main function, closure
 Lib/Fwk: Hyperapp
 Source: 
 https://github.com/hyperapp/hyperapp/blob/master/src/index.js
@@ -21,6 +21,24 @@ const actions = {
   down: value => state => ({ count: state.count - value }),
   up: value => state => ({ count: state.count + value })
 }
+/* 
+Chained arrow function = CURRYING
+Currying is transforming sum(1, 2, 3) into sum(1)(2)(3), ie a sequence of functions that only take one parameter (arity=1)
+So basically instead of:
+function up (value, state) {
+  return { 
+    count: state.count + value
+  };
+}
+Currying is:
+function up (value) {
+  return function(state) {
+    return { 
+      count: state.count + value
+    };
+  }
+}
+*/
 
 const view = (state, actions) => (
   <div>
@@ -52,4 +70,35 @@ export function app(state, actions, view, container) {
   return wiredActions
 
   // ...
+
+  // closure
+  function wireStateToActions(path, state, actions) {
+    for (var key in actions) {
+      typeof actions[key] === "function"
+        ? (function (key, action) {
+          actions[key] = function (data) {
+            if (typeof (data = action(data)) === "function") {
+              data = data(get(path, globalState), actions)
+            }
+
+            if (
+              data &&
+              data !== (state = get(path, globalState)) &&
+              !data.then // Promise
+            ) {
+              scheduleRender(
+                (globalState = set(path, clone(state, data), globalState))
+              )
+            }
+
+            return data
+          }
+        })(key, actions[key])
+        : wireStateToActions(
+          path.concat(key),
+          (state[key] = state[key] || {}),
+          (actions[key] = clone(actions[key]))
+        )
+    }
+  }
 }
